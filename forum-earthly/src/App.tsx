794f96755700441acdfb9a951c8ea48f57775824
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2026-01-21 13:21:21 +0000

    add MaxWidthWrapper component to App

--- forum-earthly/src/App.tsx
+++ forum-earthly/src/App.tsx
@@ -1,11 +1,11 @@
+import MaxWidthWrapper from "./components/MaxWidthWrapper"
 import NavBar from "./features/navbar/components/Navbar"
 
 function App() {
 
   return (
-    <>
+
       <NavBar />
-    </>
   )
 }
 
