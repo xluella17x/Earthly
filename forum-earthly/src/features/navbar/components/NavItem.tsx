2e17368f557fab4503109b8a963cdae8ce3fb701
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2026-01-21 13:20:44 +0000

    finished basic navbar styling

--- forum-earthly/src/features/navbar/components/NavItem.tsx
+++ forum-earthly/src/features/navbar/components/NavItem.tsx
@@ -1,12 +1,17 @@
+import { buttonVariants } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+
 type NavItemProps = {
-    label: string;
-    id: string;
+  label: string
+  href: string
 }
 
-function NavItem({ label, id }: NavItemProps) {
+function NavItem({ label, href }: NavItemProps) {
   return (
-    <a href={id}>
-      <div>{label}</div>
+    <a href={href}>
+      <div className={cn(buttonVariants(), "w-40 py-8")}>
+        <span className="text-lg">{label}</span>
+      </div>
     </a>
   )
 }
--- forum-earthly/src/features/navbar/components/Navbar.tsx
+++ forum-earthly/src/features/navbar/components/Navbar.tsx
@@ -1,14 +1,20 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper"
 import NavItem from "./NavItem"
 
 function NavBar() {
   return (
-    <header className="w-full p-4 bg-(--background-color) flex justify-between items-center">
-      <nav>
-        <NavItem label="Do Your Bit" id="do-your-bit" />
-        <NavItem label="Your Community" id="your-community" />
-        <NavItem label="Local Data" id="local-data" />
-        <NavItem label="Learn" id="learn" />
-      </nav>
+    <header className="w-full bg-secondary flex justify-between items-center h-20">
+      <MaxWidthWrapper className="flex flex-row justify-between items-center w-full">
+        <nav className="flex justify-center gap-2 items-center">
+          <NavItem label="Do Your Bit" href="do-your-bit" />
+          <NavItem label="Your Community" href="your-community" />
+          <NavItem label="Local Data" href="local-data" />
+          <NavItem label="Learn" href="learn" />
+        </nav>
+        <a href="/">
+          <img src="logo.png" alt="Logo" className="size-20" />
+        </a>
+      </MaxWidthWrapper>
     </header>
   )
 }
