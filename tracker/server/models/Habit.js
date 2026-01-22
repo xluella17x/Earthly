470bb49da63ebcbbf4f9635b39b9f925bc25389f
sjw-99 <samwood0312@gmail.com>
2026-01-22 16:20:17 +0000

    Experiment with calendar popup form.

--- tracker/server/models/Habit.js
+++ tracker/server/models/Habit.js
@@ -1,25 +1,58 @@
-const db = require('../db/connect');
+const db = require("../db/connect");
 
 class Habit {
-    constructor({date, user_id, postcode, commute, recycling_bags, litter_pick_bags, meat_free_day, refill_cup, second_hand_buys}) {
-        this.date = date
-        this.user_id = user_id
-        this.postcode = postcode
-        this.commute = commute
-        this.recycling_bags = recycling_bags
-        this.litter_pick_bags = litter_pick_bags
-        this.meat_free_day = meat_free_day
-        this.refill_cup = refill_cup
-        this.second_hand_buys = second_hand_buys
-    }
+  constructor({
+    date,
+    user_id,
+    postcode,
+    commute,
+    recycling_bags,
+    litter_pick_bags,
+    meat_free_day,
+    refill_cup,
+    second_hand_buys,
+  }) {
+    this.date = date;
+    this.user_id = user_id;
+    this.postcode = postcode;
+    this.commute = commute;
+    this.recycling_bags = recycling_bags;
+    this.litter_pick_bags = litter_pick_bags;
+    this.meat_free_day = meat_free_day;
+    this.refill_cup = refill_cup;
+    this.second_hand_buys = second_hand_buys;
+  }
 
-    static async create(data) {
-        const {date, user_id, postcode, commute, recycling_bags, litter_pick_bags, meat_free_day, refill_cup, second_hand_buys} = data;
+  static async create(data) {
+    const {
+      date,
+      user_id,
+      postcode,
+      commute,
+      recycling_bags,
+      litter_pick_bags,
+      meat_free_day,
+      refill_cup,
+      second_hand_buys,
+    } = data;
 
-        let response = await db.query("INSERT INTO habits_table (date, user_id, postcode, commute, recycling_bags, litter_pick_bags, meat_free_day, refill_cup, second_hand_buys) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *;", [date, user_id, postcode, commute, recycling_bags, litter_pick_bags, meat_free_day, refill_cup, second_hand_buys]);
+    let response = await db.query(
+      "INSERT INTO habits_table (date, user_id, postcode, commute, recycling_bags, litter_pick_bags, meat_free_day, refill_cup, second_hand_buys) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *;",
+      [
+        date,
+        user_id,
+        postcode,
+        commute,
+        recycling_bags,
+        litter_pick_bags,
+        meat_free_day,
+        refill_cup,
+        second_hand_buys,
+      ],
+    );
 
-        return new Habit(response.rows[0]);
-    }
+    return new Habit(response.rows[0]);
+  }
 }
 
 module.exports = Habit;
