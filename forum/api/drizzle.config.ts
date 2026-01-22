6e1c0eb486c533d8fb6803f536c7995dd57a940e
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2026-01-22 23:32:17 +0000

    Update drizzle configuration to specify migration output directory and fix db credentials url, added verbose and strict to true

--- forum/api/drizzle.config.ts
+++ forum/api/drizzle.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./src/db/drizzle/schema.ts",
-  out: "./drizzle",
+  schema: "./src/db/drizzle/schema.ts", 
+  out: "./drizzle/migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DB_URL!,
   },
+  verbose: true,
+  strict: true,
 });
