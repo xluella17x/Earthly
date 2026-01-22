ca47fa01813050165e46ebf99d21e796d07575e8
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2026-01-22 09:32:48 +0000

    replace with index.ts

--- authentication/frontend/index.ts
+++ authentication/frontend/index.ts
@@ -1,2 +1,15 @@
-console.log("Hello via Bun!");
+export default {
+  port: 3005,
+  fetch(request) {
+    const url = new URL(request.url)
+    
+    if (url.pathname === "/") {
+      return Response.redirect("./login.html")
+    }
+    
+    const file = Bun.file("." + url.pathname)
+    return new Response(file)
+  },
+}
 
+console.log("Bun server running on http://localhost:3005")
