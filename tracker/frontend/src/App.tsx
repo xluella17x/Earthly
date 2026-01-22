c8b59ca99bc3769f714071a415c501018f8a5c0b
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2026-01-22 13:57:01 +0000

    add tracking dashboard and sidebar to app

--- tracker/frontend/src/App.tsx
+++ tracker/frontend/src/App.tsx
@@ -1,12 +1,18 @@
 import "./App.css"
 import Navbar from "./features/navbar/components/Navbar"
 import WelcomeMsg from "./features/welcome-msg/components/WelcomeMsg"
+import TrackingDashboard from "./features/tracker/components/TrackingDashboard"
+import Sidebar from "./features/sidebar/components/Sidebar"
 
 function App() {
   return (
     <div className="flex flex-col gap-3">
       <Navbar />
       <WelcomeMsg />
+      <div className="flex gap-3">
+        <TrackingDashboard className="basis-2/3"/>
+        <Sidebar className="basis-1/3" />
+      </div>
     </div>
   )
 }
