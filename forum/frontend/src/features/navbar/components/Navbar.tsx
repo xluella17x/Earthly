4422d556967b51069d9d52a8d7a97c3ecbd0d5d8
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2026-01-22 13:56:53 +0000

    wrap Navbar component with SecondaryCardWrapper

--- forum/frontend/src/features/navbar/components/Navbar.tsx
+++ forum/frontend/src/features/navbar/components/Navbar.tsx
@@ -1,20 +1,23 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper"
+import SecondaryCardWrapper from "@/components/SecondaryCardWrapper"
 import NavItem from "./NavItem"
 
 function NavBar() {
   return (
-    <header className="w-full h-20 bg-secondary">
-      <MaxWidthWrapper className="flex flex-row justify-between items-center w-full">
-        <nav className="flex gap-2 items-center">
-          <NavItem label="Do Your Bit" href="do-your-bit" />
-          <NavItem label="Your Community" href="your-community" />
-          <NavItem label="Local Data" href="local-data" />
-          <NavItem label="Learn" href="learn" />
-        </nav>
-        <a href="/">
-          <img src="logo.png" alt="Logo" className="size-20 object-contain" />
-        </a>
-      </MaxWidthWrapper>
+    <header className="w-full h-20">
+      <SecondaryCardWrapper className="h-full">
+        <MaxWidthWrapper className="flex flex-row justify-between items-center w-full">
+          <nav className="flex gap-2 items-center">
+            <NavItem label="Do Your Bit" href="do-your-bit" />
+            <NavItem label="Your Community" href="your-community" />
+            <NavItem label="Local Data" href="local-data" />
+            <NavItem label="Learn" href="learn" />
+          </nav>
+          <a href="/">
+            <img src="logo.png" alt="Logo" className="size-20 object-contain" />
+          </a>
+        </MaxWidthWrapper>
+      </SecondaryCardWrapper>
     </header>
   )
 }
