0a7ab329128e88baafcfdd0265f9762bc9579948
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2026-01-22 15:12:46 +0000

    add small screen msg

--- forum/frontend/src/App.tsx
+++ forum/frontend/src/App.tsx
@@ -1,10 +1,18 @@
-import NavBar from "./features/navbar/components/Navbar"
+import "./App.css"
+import Navbar from "./features/navbar/components/Navbar"
+import SmallScreenMsg from "./components/SmallScreenMsg"
 
 function App() {
-
   return (
+    <>
+      <div className="md:hidden flex items-center justify-center absolute inset-0 mx-12">
+        <SmallScreenMsg className="" />
+      </div>
 
-      <NavBar />
+      <div className="hidden md:flex flex-col gap-6">
+        <Navbar />
+      </div>
+    </>
   )
 }
 
