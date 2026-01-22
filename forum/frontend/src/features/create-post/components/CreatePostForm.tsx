b978c2496d6e8b2cc2470e947ca925874e821899
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2026-01-22 16:13:58 +0000

    setup form without client-side functionality

--- forum/frontend/src/features/create-post/components/CreatePostForm.tsx
+++ forum/frontend/src/features/create-post/components/CreatePostForm.tsx
@@ -1,7 +1,37 @@
+import SecondaryMutedCardWrapper from "@/components/SecondaryMutedCardWrapper"
+import { useState } from "react"
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+
 const CreatePostForm = () => {
-    return ( 
-        <div>CreatePostForm Component</div>
-     );
+  const [postType, setPostType] = useState("Event")
+  const types = ["Event", "Win", "Tip"]
+
+  return (
+    <SecondaryMutedCardWrapper>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          className="rounded-lg p-2 bg-card text-card-foreground"
+          placeholder="Post Title"
+        />
+        {/* i want it to say post type as like a heading i guess and then you can select event, win or tip as like buttons rather than drop down and the selected one shows different color to others*/}
+        <div className="flex gap-2">
+          <Button variant={"secondary"}>Event</Button>
+          <Button variant={"secondary"}>Win</Button>
+          <Button variant={"secondary"}>Tip</Button>
+        </div>
+        <textarea
+          className="rounded-lg p-2 bg-card text-card-foreground"
+          placeholder="Write more details about your post here..."
+          rows={6}
+        />
+        <Button variant={"secondary"} type="submit">
+          Create Post
+        </Button>
+      </form>
+    </SecondaryMutedCardWrapper>
+  )
 }
- 
-export default CreatePostForm;
+
+export default CreatePostForm
