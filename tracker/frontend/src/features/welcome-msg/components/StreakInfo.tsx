304ed2b7cb630e3960b91668868f2fc0d9435565
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2026-01-22 12:31:29 +0000

    add streak component

--- tracker/frontend/src/features/welcome-msg/components/StreakInfo.tsx
+++ tracker/frontend/src/features/welcome-msg/components/StreakInfo.tsx
@@ -1,7 +1,20 @@
-const StreakInfo = () => {
-    return ( 
-        <></>
-     );
+const user = {
+  streak: 5,
 }
- 
-export default StreakInfo;
+
+const StreakInfo = () => {
+  return (
+    <div className="bg-secondary text-secondary-foreground flex items-center gap-1.5 rounded p-3">
+      <div className="w-50">
+        <h2 className="text-3xl">Your Green Streak</h2>
+      </div>
+
+      <div className="bg-card rounded flex items-center gap-1.5 p-1.5 pr-3">
+        <img src="logo.png" alt="logo" className="size-16" />
+        <span className="text-5xl">{user.streak} days</span>
+      </div>
+    </div>
+  )
+}
+
+export default StreakInfo
