7dc01ca68e3b14165bdf5e25ae9b4322c3596d36
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2026-01-22 12:01:16 +0000

    add logo to navbar

--- tracker/frontend/src/features/navbar/components/Navbar.tsx
+++ tracker/frontend/src/features/navbar/components/Navbar.tsx
@@ -10,10 +10,15 @@ const navItems = [
 function NavBar() {
   return (
     <header className="w-full h-20 bg-secondary rounded items-center flex px-3">
-      <nav className="flex gap-3">
-        {navItems.map((item) => (
-          <NavItem key={item.href} label={item.label} href={item.href} />
-        ))}
+      <nav className="flex justify-between w-full items-center">
+        <div className="flex gap-3">
+          {navItems.map((item) => (
+            <NavItem key={item.href} label={item.label} href={item.href} />
+          ))}
+        </div>
+        <a href="/">
+          <img src="logo.png" alt="Logo" className="size-20 object-contain" />
+        </a>
       </nav>
     </header>
   )
