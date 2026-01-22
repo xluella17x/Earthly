e7fa0bea3217f3da6667cfd47b2e5fdd1ff91108
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2026-01-22 15:01:03 +0000

    add whitespace break spaces so that navitems text is  less squished

--- tracker/frontend/src/features/navbar/components/NavItem.tsx
+++ tracker/frontend/src/features/navbar/components/NavItem.tsx
@@ -11,7 +11,7 @@ type NavItemProps = {
 function NavItem({ label, href }: NavItemProps) {
   return (
     <a href={href}>
-      <div className={cn(buttonVariants({ variant: "card" }), "w-40 py-8")}>
+      <div className={cn(buttonVariants({ variant: "card" }), "w-40 py-8 whitespace-break-spaces")}>
         <span className="text-lg font-medium">{label}</span>
       </div>
     </a>
