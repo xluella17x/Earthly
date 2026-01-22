d05b90e3eaee2724483b52b1d8641b8e6e806f20
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2026-01-22 12:14:48 +0000

    add WelcomeMsg component to App

--- tracker/frontend/src/App.tsx
+++ tracker/frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css"
 import Navbar from "./features/navbar/components/Navbar"
+import WelcomeMsg from "./features/welcome-msg/components/WelcomeMsg"
 
 function App() {
   return (
     <>
         <Navbar />
+        <WelcomeMsg />
     </>
   )
 }
