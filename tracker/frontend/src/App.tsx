40dff1ec1d9e19e5b50615039cbdca979237be85
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2026-01-22 15:03:33 +0000

    add small screen message to small screen sizes

--- tracker/frontend/src/App.tsx
+++ tracker/frontend/src/App.tsx
@@ -3,17 +3,21 @@ import Navbar from "./features/navbar/components/Navbar"
 import WelcomeMsg from "./features/welcome-msg/components/WelcomeMsg"
 import TrackingDashboard from "./features/tracker/components/TrackingDashboard"
 import Sidebar from "./features/sidebar/components/Sidebar"
+import SmallScreenMsg from "./components/SmallScreenMsg"
 
 function App() {
   return (
-    <div className="hidden md:flex flex-col gap-6">
-      <Navbar />
-      <WelcomeMsg />
-      <div className="flex gap-3">
-        <TrackingDashboard className="basis-2/3"/>
-        <Sidebar className="basis-1/3" />
+    <>
+      <SmallScreenMsg className="md:hidden" />
+      <div className="hidden md:flex flex-col gap-6">
+        <Navbar />
+        <WelcomeMsg />
+        <div className="flex gap-3">
+          <TrackingDashboard className="basis-2/3" />
+          <Sidebar className="basis-1/3" />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
