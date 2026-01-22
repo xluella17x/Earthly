c353b3259fcf179c325bf0a508ad4297624a612c
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2026-01-22 11:23:08 +0000

    copied navbar from forum to tracker

---
+++ tracker/frontend/src/features/navbar/components/NavItem.tsx
@@ -0,0 +1,19 @@
+import { buttonVariants } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+
+type NavItemProps = {
+  label: string
+  href: string
+}
+
+function NavItem({ label, href }: NavItemProps) {
+  return (
+    <a href={href}>
+      <div className={cn(buttonVariants(), "w-40 py-8")}>
+        <span className="text-lg font-medium">{label}</span>
+      </div>
+    </a>
+  )
+}
+
+export default NavItem
---
+++ tracker/frontend/src/features/navbar/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper"
+import NavItem from "./NavItem"
+
+function NavBar() {
+  return (
+    <header className="w-full h-20 bg-secondary">
+      <MaxWidthWrapper className="flex flex-row justify-between items-center w-full">
+        <nav className="flex gap-2 items-center">
+          <NavItem label="Do Your Bit" href="do-your-bit" />
+          <NavItem label="Your Community" href="your-community" />
+          <NavItem label="Local Data" href="local-data" />
+          <NavItem label="Learn" href="learn" />
+        </nav>
+        <a href="/">
+          <img src="logo.png" alt="Logo" className="size-20 object-contain" />
+        </a>
+      </MaxWidthWrapper>
+    </header>
+  )
+}
+export default NavBar
