8e52c92b37fd847c38c007d4c62f42d92692d3b5
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2026-01-23 01:02:01 +0000

    added post type enum

--- forum/api/src/db/drizzle/schema/post.ts
+++ forum/api/src/db/drizzle/schema/post.ts
@@ -1,15 +1,19 @@
-import { pgTable, text, doublePrecision } from "drizzle-orm/pg-core"
+import { pgTable, text, doublePrecision, pgEnum } from "drizzle-orm/pg-core"
 import { relations } from "drizzle-orm"
 import { id, createdAt, updatedAt } from "../schemaHelpers"
 import { PostLikeTable } from "./postLike"
 import { PostAttendeeTable } from "./postAttendee"
+
+export const postTypes = ["event", "discussion", "announcement"] as const
+export type PostType = (typeof postTypes)[number]
+export const postTypeEnum = pgEnum("post_type", postTypes)
 
 export const PostTable = pgTable("posts_table", {
   id,
   userId: text("user_id").notNull(), 
   title: text("title").notNull(),
   description: text("description").notNull(),
-  type: text("type").notNull(),
+  type: postTypeEnum("type").notNull(),
   imageUrl: text("image_url"),
   locationName: text("location_name"),
   lat: doublePrecision("latitude"),
