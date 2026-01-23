e08d40c0ee7fe4618ed52279cd1f2db1bcac0d81
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2026-01-23 00:46:13 +0000

    update output for migrations location

--- forum/api/drizzle.config.ts
+++ forum/api/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   schema: "./src/db/drizzle/schema.ts", 
-  out: "./drizzle/migrations",
+  out: "./src/db/drizzle/migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DB_URL!,
