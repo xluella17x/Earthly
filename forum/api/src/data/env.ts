079af9662f61401dd185364868094f63b6a72893
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2026-01-23 09:38:32 +0000

    fix env.ts to use db_url

--- forum/api/src/data/env.ts
+++ forum/api/src/data/env.ts
@@ -3,11 +3,7 @@ import { z } from "zod"
 
 export const env = createEnv({
   server: {
-    DB_PASSWORD: z.string().min(1),
-    DB_USER: z.string().min(1),
-    DB_NAME: z.string().min(1),
-    DB_HOST: z.string().min(1),
-    DB_PORT: z.coerce.number().default(5432), 
+    DB_URL: z.url()
   },
   
   runtimeEnv: process.env,
