49490c8110a43a3b20c63dd2818ffa9d7baa5048
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2026-01-27 11:21:02 +0000

    specify id field name

--- forum/api/src/db/drizzle/schemaHelpers.ts
+++ forum/api/src/db/drizzle/schemaHelpers.ts
@@ -1,11 +1,9 @@
 import { timestamp, uuid } from "drizzle-orm/pg-core"
 
-export const id = uuid().primaryKey().defaultRandom()
-export const createdAt = timestamp("created_at") 
-  .notNull()
-  .defaultNow();
+export const id = uuid("id").primaryKey().defaultRandom()
+export const createdAt = timestamp("created_at").notNull().defaultNow()
 
-export const updatedAt = timestamp("updated_at") 
+export const updatedAt = timestamp("updated_at")
   .notNull()
   .defaultNow()
-  .$onUpdate(() => new Date());
+  .$onUpdate(() => new Date())
