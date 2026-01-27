1c66cd40dc7b4299dce264a27906ba1a60492bb5
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2026-01-27 10:43:54 +0000

    re-generated db
    Co-authored-by: ramonaM26 <ramonaM26@users.noreply.github.com>

--- forum/api/src/db/drizzle/migrations/0000_skinny_vermin.sql
+++ forum/api/src/db/drizzle/migrations/0000_skinny_vermin.sql
@@ -1,30 +1,35 @@
+CREATE TYPE "public"."post_status" AS ENUM('draft', 'published', 'archived');--> statement-breakpoint
+CREATE TYPE "public"."post_type" AS ENUM('event', 'discussion', 'announcement');--> statement-breakpoint
+CREATE TYPE "public"."post_attendee_status" AS ENUM('going', 'interested', 'not_going');--> statement-breakpoint
 CREATE TABLE "posts_table" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"user_id" text NOT NULL,
 	"title" text NOT NULL,
 	"description" text NOT NULL,
-	"type" text NOT NULL,
+	"type" "post_type" NOT NULL,
 	"image_url" text,
+	"status" "post_status" NOT NULL,
 	"location_name" text,
 	"latitude" double precision,
 	"longitude" double precision,
-	"createdAt" timestamp with time zone DEFAULT now() NOT NULL,
-	"updatedAt" timestamp with time zone DEFAULT now() NOT NULL
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
+	"deleted_at" timestamp with time zone
 );
 --> statement-breakpoint
 CREATE TABLE "post_likes_table" (
 	"post_id" uuid NOT NULL,
 	"user_id" text NOT NULL,
-	"createdAt" timestamp with time zone DEFAULT now() NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
 	CONSTRAINT "post_likes_table_user_id_post_id_pk" PRIMARY KEY("user_id","post_id")
 );
 --> statement-breakpoint
 CREATE TABLE "post_attendees_table" (
 	"post_id" uuid NOT NULL,
 	"user_id" text NOT NULL,
-	"status" text DEFAULT 'going' NOT NULL,
-	"createdAt" timestamp with time zone DEFAULT now() NOT NULL,
-	"updatedAt" timestamp with time zone DEFAULT now() NOT NULL,
+	"status" "post_attendee_status" DEFAULT 'not_going' NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL,
 	CONSTRAINT "post_attendees_table_user_id_post_id_pk" PRIMARY KEY("user_id","post_id")
 );
 --> statement-breakpoint
--- forum/api/src/db/drizzle/migrations/0002_magical_ted_forrester.sql
+++
@@ -1,6 +0,0 @@
-CREATE TYPE "public"."post_status" AS ENUM('draft', 'published', 'archived');--> statement-breakpoint
-CREATE TYPE "public"."post_attendee_status" AS ENUM('going', 'interested', 'not_going');--> statement-breakpoint
-ALTER TABLE "post_attendees_table" ALTER COLUMN "status" SET DEFAULT 'not_going'::"public"."post_attendee_status";--> statement-breakpoint
-ALTER TABLE "post_attendees_table" ALTER COLUMN "status" SET DATA TYPE "public"."post_attendee_status" USING "status"::"public"."post_attendee_status";--> statement-breakpoint
-ALTER TABLE "posts_table" ADD COLUMN "status" "post_status" NOT NULL;--> statement-breakpoint
-ALTER TABLE "posts_table" ADD COLUMN "deletedAt" timestamp with time zone;
