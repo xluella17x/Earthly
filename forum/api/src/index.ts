badff2b6191b660796ece1da7a7dc43a225f4010
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2026-01-27 12:34:30 +0000

    export app from index.ts and add test for GET /

--- forum/api/src/index.ts
+++ forum/api/src/index.ts
@@ -3,12 +3,14 @@ import { postsRoutes } from "./routes/posts"
 import { swagger } from "@elysiajs/swagger"
 import { cors } from "@elysiajs/cors"
 
-const app = new Elysia()
+export const app = new Elysia()
   .use(swagger())
   .use(cors())
   .use(postsRoutes)
   .get("/", () => "Welcome to the Forum Service!")
-  .listen(5174)
-  
 
-console.log(`🦊 Forum Service running at ${app.server?.hostname}:${app.server?.port}`)
+ 
+if (import.meta.main) {
+  app.listen(5174)
+  console.log(`🦊 Forum Service running at ${app.server?.hostname}:${app.server?.port}`)
+}
