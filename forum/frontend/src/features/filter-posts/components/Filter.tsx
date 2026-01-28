17206c38fb29b856975f3304f0cb578e84980a43
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2026-01-28 10:37:12 +0000

    define FilterProps interface

--- forum/frontend/src/features/filter-posts/components/Filter.tsx
+++ forum/frontend/src/features/filter-posts/components/Filter.tsx
@@ -3,6 +3,9 @@ import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
 import { POST_TYPE_COLORS, POST_TYPES } from "@/lib/constants"
 
+interface FilterProps {
+  currentFilter: string | undefined
+  onFilterChange: (filter: string | undefined) => void
 }
  
 export default Filter;
