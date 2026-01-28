219b49fd6a8156143be7408b6ccce960ac6ec371
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2026-01-28 13:02:00 +0000

    add missing timezone declaration

--- forum/api/src/db/drizzle/schemaHelpers.ts
+++ forum/api/src/db/drizzle/schemaHelpers.ts
@@ -1,9 +1,12 @@
 import { timestamp, uuid } from "drizzle-orm/pg-core"
 
-export const id = uuid("id").primaryKey().defaultRandom()
-export const createdAt = timestamp("created_at").notNull().defaultNow()
-
-export const updatedAt = timestamp("updated_at")
+export const id = uuid().primaryKey().defaultRandom()
+export const createdAt = timestamp("created_at", { withTimezone: true })
+  .notNull()
+  .defaultNow()
+export const updatedAt = timestamp("updated_at", { withTimezone: true })
   .notNull()
   .defaultNow()
   .$onUpdate(() => new Date())
+
+export const deletedAt = timestamp("deleted_at", { withTimezone: true })
